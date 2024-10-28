886055b376a64667e8d2cc7aa859f4a9f29f7c77
Manatik <mml21122012@gmail.com>
2024-10-28 03:16:36 +0300

    init v2

--- backend/src/app/role/dto/update-role.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsOptional, IsString } from 'class-validator';
-
-export class UpdateRoleDto {
-  @IsOptional()
-  @IsString()
-  name: string;
-}
---
+++ packages/backend/src/app/user/dto/update-user.dto.ts
@@ -0,0 +1,28 @@
+import { Transform } from 'class-transformer';
+import { IsDateString, IsOptional, IsPhoneNumber, IsString } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @IsPhoneNumber('RU')
+  phone: string;
+
+  @IsOptional()
+  @IsString()
+  firstname: string;
+
+  @IsOptional()
+  @IsString()
+  @Transform(({ value }) => (!value ? undefined : value))
+  lastname: string;
+
+  @IsOptional()
+  @IsString()
+  @Transform(({ value }) => (!value ? undefined : value))
+  @IsDateString()
+  birthdate: Date;
+
+  @IsOptional()
+  @IsString({ each: true })
+  roles: string[];
+}
